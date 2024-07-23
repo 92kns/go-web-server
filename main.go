36fdf1422a6141f316be5a21b3e28e00f9937700
KS <KS@random.local>
2024-07-23 17:53:20 -0400

    Format prints

--- main.go
+++ main.go
@@ -12,10 +12,12 @@ func main() {
 	// CLI arguments. We just need `port`, `host`, and `dir` (directory) of html files to serve.
 	// Default to localhost:8080 and cwd
 	port := flag.Int("port", 8080, "Port to listen on")
-	// host := flag.String("host", "127.0.0.1", "Host to listen on")
-	host := "127.0.01"
 	dir := flag.String("dir", ".", "Directory to serve")
 	flag.Parse()
+
+	// For now just default to localhost
+	// host := flag.String("host", "127.0.0.1", "Host to listen on")
+	host := "127.0.01"
 
 	// Verify directory of benchmark files to serve exist and then setup the file server
 	if _, err := os.Stat(*dir); os.IsNotExist(err) {
@@ -26,7 +28,7 @@ func main() {
 
 	// If everything is OK, we can start the server and host the files
 	address := fmt.Sprintf("%s:%d", host, *port)
-	log.Printf("Serving %s on HTTP %s:%d\n", *dir, address)
+	log.Printf("Serving %s on HTTP %s\n", *dir, address)
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal(err)
