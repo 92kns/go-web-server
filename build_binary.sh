5b47caff755f798ffb6e06605414d0105f37a3b8
KS <KS@random.local>
2024-07-23 17:13:16 -0400

    change build script output

--- build_binary.sh
+++ build_binary.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 SOURCE_FILE="main.go"
-OUTPUT_BASE="http"
+OUTPUT_BASE="gws" #go web server
 OUTPUT_DIR="./bin/"
 
 # Assume Go is installed
 
 # Build for Apple Silicon (arm64)
-GOOS=darwin GOARCH=arm64 go build -o "${OUTPUT_DIR}${OUTPUT_BASE}_darwin_arm64" $SOURCE_FILE
-echo "Built ${OUTPUT_BASE}_darwin_arm64"
+GOOS=darwin GOARCH=arm64 go build -o "${OUTPUT_DIR}darwin_arm64/${OUTPUT_BASE}" $SOURCE_FILE
+echo "Built ${OUTPUT_BASE} for ${GOOS}_${GOARCH}"
 
 # Build for Apple Intel 64-bit (amd64)
-GOOS=darwin GOARCH=amd64 go build -o "${OUTPUT_DIR}${OUTPUT_BASE}_darwin_amd64" $SOURCE_FILE
-echo "Built ${OUTPUT_BASE}_darwin_amd64"
+GOOS=darwin GOARCH=amd64 go build -o "${OUTPUT_DIR}darwin_amd64/${OUTPUT_BASE}" $SOURCE_FILE
+echo "Built ${OUTPUT_BASE} for ${GOOS}_${GOARCH}"
 
 # Build for Win 11 64-bit (amd64)
-GOOS=windows GOARCH=amd64 go build -o "${OUTPUT_DIR}${OUTPUT_BASE}_windows_amd64.exe" $SOURCE_FILE
-echo "Built ${OUTPUT_BASE}_windows_amd64.exe"
+GOOS=windows GOARCH=amd64 go build -o "${OUTPUT_DIR}windows_amd64/${OUTPUT_BASE}.exe" $SOURCE_FILE
+echo "Built ${OUTPUT_BASE} for ${GOOS}_${GOARCH}"
 
 # Build for Linux 64-bit (amd64)
-GOOS=linux GOARCH=amd64 go build -o "${OUTPUT_DIR}${OUTPUT_BASE}_linux_amd64" $SOURCE_FILE
-echo "Built ${OUTPUT_BASE}_linux_amd64"
+GOOS=linux GOARCH=amd64 go build -o "${OUTPUT_DIR}linux_amd64/${OUTPUT_BASE}" $SOURCE_FILE
+echo "Built ${OUTPUT_BASE} for "${GOOS}"_"${GOARCH}""
 
 echo "Builds completed! Check the $OUTPUT_DIR directory."
